8898be9c12777a6d8f16babea9d7f414ef091bfd
tkurz <th.kurz@gmail.com>
2014-03-20 15:09:48 +0100

    to fix

--- squebi/js/squebi.js
+++ squebi/js/squebi.js
@@ -40,7 +40,7 @@ squebi.service("$extension", function ($rootScope) {
         //order
         extension.resultWriter = extension.resultWriter.sort(function(a,b) {
             return a <= b ? -1 : 1;
-        });
+        });console.log(extension.resultWriter)
         return extension.resultWriter;
     }
 
