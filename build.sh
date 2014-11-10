18167978351c562def0542719c69ce7b360b3e33
Thomas Kurz <thomas.kurz@salzburgresearch.at>
2014-11-10 19:09:43 +0100

    enable uglify

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo Merge css files into dist/squebi.css
 node r.js -o optimizeCss=standard.keepLines.keepWhitespace cssIn=squebi/css/main.css out=squebi.css
 
 echo Merge and uglify javascript files into squebi.js
-node r.js -o baseUrl=. mainConfigFile=main.js name=main out=squebi.js optimize=none
+node r.js -o baseUrl=. mainConfigFile=main.js name=main out=squebi.js optimize=uglify
 
 #echo Move to dist
 mkdir dist
