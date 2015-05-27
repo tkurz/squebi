5495592e3a92515957d5c504b610172ece3a3211
Thomas Kurz <thomas.kurz@salzburgresearch.at>
2015-05-27 12:57:08 +0200

    remove uglification because it produces corrupt code

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo Merge css files into dist/squebi.css
 node r.js -o optimizeCss=standard.keepLines.keepWhitespace cssIn=squebi/css/main.css out=squebi.css
 
 echo Merge and uglify javascript files into squebi.js
-node r.js -o baseUrl=. mainConfigFile=main.js name=main out=squebi.js optimize=uglify
+node r.js -o baseUrl=. mainConfigFile=main.js name=main out=squebi.js optimize=none
 
 #echo Move to dist
 mkdir dist
